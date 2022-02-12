b14bd65dee6d6a842b43456ef455469455ac8883
suhaiLap-hp <suhailm9055@gmail.com>
2022-02-12 23:35:09 +0530

    db connection

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const app= express();
-const mongoose= require("mongoose")
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 
-app.listen(5000,()=>{
-    console.log("Server is running...");
+mongoose.connect(
+  process.env.MONGO_URL)
+  .then(()=>console.log("DB Connection successful!"))
+.catch((err)=>{
+    console.log(err);
 })
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is running...");
+});
